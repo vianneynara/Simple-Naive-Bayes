b5ce78c19805db2c711dbf1465ec716012869098
narwa <narwandaru.nara@gmail.com>
2025-03-22 01:25:51 +0700

    test: manual test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,33 +9,46 @@ public class Main {
 
 		Dataset dataset = reader.readCSV(pathToDatset);
 
-		// print the dataset metadata
-		for (var feature : dataset.metadata().entrySet()) {
-			System.out.println("Feature: " + feature.getKey());
-			for (var frequencyEntry : feature.getValue().entrySet()) {
-				System.out.printf("%12s : %d %n", frequencyEntry.getKey(), frequencyEntry.getValue());
-			}
-			System.out.println("=".repeat(50));
-		}
-
-		System.out.println("\n".repeat(3));
-
+//		// print the dataset metadata
+//		for (var feature : dataset.metadata().entrySet()) {
+//			System.out.println("Feature: " + feature.getKey());
+//			for (var frequencyEntry : feature.getValue().entrySet()) {
+//				System.out.printf("%12s : %d %n", frequencyEntry.getKey(), frequencyEntry.getValue());
+//			}
+//			System.out.println("=".repeat(50));
+//		}
+//
+//		System.out.println("\n".repeat(3));
+//
 		NaiveBayes nb = new NaiveBayes(dataset);
+//
+//		// print the correlations
+//		Map<String, Map<String, Map<String, Integer>>> yCorrelations = nb.readCorrelations("buys_computer");
+//		for (var yValuesEntry : yCorrelations.entrySet()) {
+//			System.out.printf("[Y] [%s]: %n", yValuesEntry.getKey());
+//			for (var xValuesEntry : yValuesEntry.getValue().entrySet()) {
+//				System.out.printf("\t(X) (%s): %n", xValuesEntry.getKey());
+//				for (var xValuesOccurrences : xValuesEntry.getValue().entrySet()) {
+//					System.out.printf("\t\t- %-10s : %d %n", xValuesOccurrences.getKey(), xValuesOccurrences.getValue());
+//				}
+//			}
+//			System.out.println("=".repeat(50));
+//		}
+//
+//		double probability = nb.computeProbability(yCorrelations, "age", "31...40", "buys_computer", "yes");
+//		System.out.printf("Probability: %.4f %n", probability);
 
-		Map<String, Map<String, Map<String, Integer>>> yCorrelations = nb.readCorrelations("buys_computer");
+		/* The actual test based on manual calculation */
 
-		for (var yValuesEntry : yCorrelations.entrySet()) {
-			System.out.printf("[Y] [%s]: %n", yValuesEntry.getKey());
-			for (var xValuesEntry : yValuesEntry.getValue().entrySet()) {
-				System.out.printf("\t(X) (%s): %n", xValuesEntry.getKey());
-				for (var xValuesOccurrences : xValuesEntry.getValue().entrySet()) {
-					System.out.printf("\t\t- %-10s : %d %n", xValuesOccurrences.getKey(), xValuesOccurrences.getValue());
-				}
-			}
-			System.out.println("=".repeat(50));
-		}
-
-		double probability = nb.computeProbability(yCorrelations, "age", "31...40", "buys_computer", "yes");
-		System.out.printf("Probability: %.4f", probability);
+		Map<String, String> X = Map.of(
+			"age", "31...40",
+			"income", "low",
+			"student", "no",
+			"credit_rating", "fair"
+		);
+		String target = "buys_computer";
+		Result result = nb.predict(X, target, true);
+		System.out.printf("Classification (%s): %s (%.2f %%)",
+			target, result.classification(), result.predictions().get(result.classification()) * 100);
 	}
 }
