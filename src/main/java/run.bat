22723a3424846856b82cc8567092ffa0398e6556
narwa <narwandaru.nara@gmail.com>
2025-03-22 02:51:01 +0700

    compiler and runner for non gradle

---
+++ src/main/java/run.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Navigate to the directory containing Main.java
+cd /d D:\Development\Java\Simple-Naive-Bayes\src\main\java
+
+REM Compile all Java files in the directory
+javac *.java
+
+REM Run the Main class
+java Main
